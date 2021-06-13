b8c646a26c29c3a7364609e3b7a869409b26fabf
aadeeteeya <aadeeteeya99@gmail.com>
2021-06-14 00:30:30 +0530

    updated README and added exception blocks

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # Bitcoin Block Constructor
 
-todo
+Given: mempool.csv with `<txid>,<fee>,<weight>,<parent_txids>` format
+
+Output: block.txt with `<txid>` format
+
+<br>
+<br>
+
+### Some Assumptions
+
+1. All txids are unique and not repeated
+2. Only considering tx given in the mempool set (and not before the set or after the set)
+3. Miner does not have a limit on how many tx he needs to select 
+4. The mempool.csv is not massive
+
+### About the Solution
+
+1. This solution uses a hashmap implementation (for the python implementation I've used build in type dictonary),
+2. A hashmap `cache_tx` stores all the txids and parent_ids as `txid: parent_ids`,
+3. Since all the parents_ids should occur before the txid we check if all the parent_ids exist before or not in `cache_tx`,
+4. And for the O(1) lookup time we tradeoff O(n) space (which means the dictionary will fail if the mempool.csv is larger than free memory available on the system).
--- block.py
+++ block.py
@@ -4,27 +4,35 @@ def __main__() :
     cache_tx = {}
     result = []
 
-    def mempool_tx(txid, fee, weight, parents) :
-        if int(weight) < 4000000 :
-            if txid in cache_tx :
-                print("repeated:")
-                print(txid)
-            cache_tx[txid] = parents
-            is_valid = parents != "" and parents in cache_tx
-            if is_valid :
-                result.append(txid)
+    def mempool_tx(txid, fee, weight, parents_ids) :
+        try :
+            if int(weight) < 4000000 :
+                if txid in cache_tx :
+                    print("repeated:")
+                    print(txid)
+                cache_tx[txid] = parents_ids
+
+                # To check if the tx is valid or not
+                is_valid = parents_ids != "" and all(parent in cache_tx for parent in parents_ids.split(';'))
+                if is_valid :
+                    result.append(txid)
+        except ValueError as error:
+            print('Error occured:', error)
+
 
     def parse_mempool_csv():
-        with open('mempool.csv') as f:
-            # To skip the csv headers
-            next(f)
-            for line in f.readlines() :
-                mempool_tx(*line.strip().split(','))
-    
+        try :
+            with open('mempool.csv') as f:
+                # To skip the csv headers
+                next(f)
+                for line in f.readlines() :
+                    mempool_tx(*line.strip().split(','))
+        except IOError as error:
+            print('Error: ', error)
+
     
     parse_mempool_csv()
     print(len(result))
-    print(len(cache_tx))
 
 if __name__ == '__main__' :
     __main__()
