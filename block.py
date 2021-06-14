f9068bedb37b0b1dbc5902ded87a7877d2cad068
aadeeteeya <aadeeteeya99@gmail.com>
2021-06-14 10:31:13 +0530

    added write functionality

--- block.py
+++ block.py
@@ -1,38 +1,38 @@
 #!/usr/bin/python3
 
+
 def __main__() :
     cache_tx = {}
-    result = []
-
+    
     def mempool_tx(txid, fee, weight, parents_ids) :
         try :
             if int(weight) < 4000000 :
-                if txid in cache_tx :
-                    print("repeated:")
-                    print(txid)
                 cache_tx[txid] = parents_ids
 
                 # To check if the tx is valid or not
                 is_valid = parents_ids != "" and all(parent in cache_tx for parent in parents_ids.split(';'))
                 if is_valid :
-                    result.append(txid)
+                    return txid
+                
         except ValueError as error:
             print('Error occured:', error)
-
-
+    
     def parse_mempool_csv():
         try :
-            with open('mempool.csv') as f:
+            with open('mempool.csv') as mempool_file, open('block.txt', 'w') as result_file :
                 # To skip the csv headers
-                next(f)
-                for line in f.readlines() :
-                    mempool_tx(*line.strip().split(','))
+                next(mempool_file)
+                for line in mempool_file.readlines() :
+                    result = mempool_tx(*line.strip().split(','))
+                    
+                    if result :
+                        result_file.write(result + '\n')
+                    
         except IOError as error:
             print('Error: ', error)
 
     
     parse_mempool_csv()
-    print(len(result))
 
 if __name__ == '__main__' :
     __main__()
